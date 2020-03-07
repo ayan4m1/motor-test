74ce439b785633dbaea088f250ad9d687bb07c6b
ayan4m1 <andrew@bulletlogic.com>
2020-03-07 09:57:58 -0500

    add some info and set pin numbers correctly

--- src/index.js
+++ src/index.js
@@ -29,12 +29,17 @@ const sweepLoop = () => {
 
 board.on("ready", () => {
   // use pins 1-4 to control four servos
+  // servos[0] = front-left
+  // servos[1] = front-right
+  // servos[2] = rear-left
+  // servos[3] = rear-right
   const servos = new five.Servos([
-    makeServo(1), // front-left
-    makeServo(2), // front-right
-    makeServo(3), // rear-left
-    makeServo(4) // rear-right
+    makeServo("P1-32"), // front-left
+    makeServo("P1-33"), // front-right
+    makeServo("P1-12"), // rear-left
+    makeServo("P1-35") // rear-right
   ]);
 
+  // start an infinite loop of on-off cycles
   sweepLoop();
 });
